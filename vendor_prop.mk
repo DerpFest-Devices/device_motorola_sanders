604143e0aa3a23110e547c8422c9ac9a931c530a
Shalaj Jain <shalajj@codeaurora.org>
2018-10-07 05:51:11 +0000

    sanders: Enable Smooth Streaming for secure video playback
    Re-purpose previously unused property and enable smooth
    streaming for secure video playback.
    
    Signed-off-by: Mayur <ultramayur123@gmail.com>

--- vendor_prop.mk
+++ vendor_prop.mk
@@ -237,8 +237,8 @@ PRODUCT_PROPERTY_OVERRIDES += \
     media.aac_51_output_enabled=true \
     media.settings.xml=/vendor/etc/media_profiles.xml \
     mm.enable.qcom_parser=135715 \
-    mm.enable.sec.smoothstreaming=false \
-    mm.enable.smoothstreaming=false \
+    mm.enable.sec.smoothstreaming=true \
+    mm.enable.smoothstreaming=true \
     mmp.enable.3g2=true
 
 # UI
