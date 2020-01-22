63d73db13e2a7472a59a22e37773c80254094b39
marshmello61 <ultramayur123@gmail.com>
2020-01-22 07:49:54 +0000

    Revert "Sanders: Enforce RRO only for framework-res"
    This reverts commit f2077044f33e09c9e264f985af6f8617902251ea.

--- device.mk
+++ device.mk
@@ -321,9 +321,6 @@ PRODUCT_COPY_FILES += \
      $(LOCAL_PATH)/configs/perf/targetconfig.xml:$(TARGET_COPY_OUT_VENDOR)/etc/perf/targetconfig.xml \
      $(LOCAL_PATH)/configs/perf/targetresourceconfigs.xml:$(TARGET_COPY_OUT_VENDOR)/etc/perf/targetresourceconfigs.xml
 
-PRODUCT_ENFORCE_RRO_TARGETS := \
-    framework-res
-
 # Perf configs
 PRODUCT_COPY_FILES += \
      $(LOCAL_PATH)/configs/perf/commonresourceconfigs.xml:$(TARGET_COPY_OUT_VENDOR)/etc/perf/commonresourceconfigs.xml \
