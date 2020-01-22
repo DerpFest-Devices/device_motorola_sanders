80a378220b79013bf4ca50bbe4e5f22f25faeeab
marshmello61 <ultramayur123@gmail.com>
2020-01-22 09:31:15 +0000

    Revert "sanders: Use 4GB dalvik-heap config"
    This reverts commit 3eba0bd2f603882c3608b501509d325328f2fa89.
    
    Signed-off-by: marshmello61 <ultramayur123@gmail.com>

--- device.mk
+++ device.mk
@@ -19,9 +19,6 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
 # Vendor properties
 -include device/motorola/sanders/vendor_prop.mk
 
-# Setup dalvik vm configs
-$(call inherit-product, frameworks/native/build/phone-xhdpi-4096-dalvik-heap.mk)
-
 # These are the hardware-specific features
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.bluetooth.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.bluetooth.xml \
