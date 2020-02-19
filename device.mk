77d3fdb87bcd17a8f77ddf239a47e462838f35fd
Mayur <ultramayur123@gmail.com>
2020-02-19 12:48:17 +0000

    Sanders: prebuilt: remove Via and add OperaMini
    Signed-off-by: Mayur <ultramayur123@gmail.com>

--- device.mk
+++ device.mk
@@ -490,9 +490,9 @@ PRODUCT_PACKAGES += \
 
 # Prebuilt Packages
 PRODUCT_PACKAGES += \
-    Via \
+    OperaMini \
     Wallpapers
-    
+
 PRODUCT_BUILD_PROP_OVERRIDES += BUILD_UTC_DATE=0
 
 PRODUCT_GMS_CLIENTID_BASE := android-motorola
