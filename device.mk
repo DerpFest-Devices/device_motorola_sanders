51602a1d58dc5679c03d7dc1503c48d2cc183b84
Melvin <kartik3163@gmail.com>
2020-03-20 23:18:11 +0530

    sanders: Import configstore@1.1.policy from Daisy Q
    Signed-off-by: Melvin <kartik3163@gmail.com>
    Signed-off-by: Mayur <ultramayur123@gmail.com>

--- device.mk
+++ device.mk
@@ -411,6 +411,7 @@ PRODUCT_PACKAGES += \
 
 # Seccomp policy
 PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/seccomp_policy/configstore@1.1.policy:$(TARGET_COPY_OUT_VENDOR)/etc/seccomp_policy/configstore@1.1.policy \
     $(LOCAL_PATH)/seccomp_policy/mediacodec.policy:$(TARGET_COPY_OUT_VENDOR)/etc/seccomp_policy/mediacodec.policy \
     $(LOCAL_PATH)/seccomp_policy/mediaextractor.policy:$(TARGET_COPY_OUT_VENDOR)/etc/seccomp_policy/mediaextractor.policy
 
