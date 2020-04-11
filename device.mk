128f77a261dbdfe44296c2ceda34b73c85955197
Mayur <ultramayur123@gmail.com>
2020-04-11 07:27:45 +0000

    Revert "sanders: use local data-ipa-cfg-mgr"
    This reverts commit dd7edd34cf1fc014331ac85e72e205083619250f.

--- device.mk
+++ device.mk
@@ -208,9 +208,6 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/gps/etc/sap.conf:$(TARGET_COPY_OUT_VENDOR)/etc/sap.conf \
     $(LOCAL_PATH)/gps/etc/xtwifi.conf:$(TARGET_COPY_OUT_VENDOR)/etc/xtwifi.conf
 
-# IPA
-USE_DEVICE_SPECIFIC_DATA_IPA_CFG_MGR := true
-
 # health
 PRODUCT_PACKAGES += \
     android.hardware.health@2.0-impl \
