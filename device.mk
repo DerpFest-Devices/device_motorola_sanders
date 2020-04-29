bb6506c34f72c6b4bd6eba011784b019d691522e
Mayur <ultramayur123@gmail.com>
2020-04-29 15:13:31 +0000

    sanders: prebuilt: add QuickPick Gallery  *also override the Gallery2
    Signed-off-by: Mayur <ultramayur123@gmail.com>

--- device.mk
+++ device.mk
@@ -525,7 +525,8 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     Via \
     Wallpapers \
-    SmsOrganizer
+    SmsOrganizer \
+    QPGallery
 
 PRODUCT_BUILD_PROP_OVERRIDES += BUILD_UTC_DATE=0
 
